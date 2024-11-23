cf293359238b16d4418290f9bdc250be97ae2fb6
aaryannemade <a.nemade@outlook.com>
2024-11-23 16:37:03 +0100

    Set up extenal links on the board

---
+++ externalLinks.js
@@ -0,0 +1,69 @@
+﻿//Youtube: Baba is You Trailer
+var southWestCord = L.latLng(79.92439, 90.28564);
+var northEatCord = L.latLng(81.305, 101.64551);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://youtu.be/Ca5BYKRVXHM?si=QhKnwWDjM5MJ1q6V", '_blank').focus();
+});
+
+//Arvi Portfolio
+var southWestCord = L.latLng(79.92824, 112.32422);
+var northEatCord = L.latLng(81.31164, 119.27856);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.hempuli.com/", '_blank').focus();
+});
+
+//Podcast
+var southWestCord = L.latLng(79.92824, 120.146);
+var northEatCord = L.latLng(81.31164, 126.628);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://podcasts.apple.com/us/podcast/baba-is-you-creator-arvi-teikari/id1313004515?i=1000476387204", '_blank').focus();
+});
+
+//Baba is You Portfolio
+var southWestCord = L.latLng(79.92824, 127.353);
+var northEatCord = L.latLng(81.31164, 132.186);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://hempuli.com/baba/", '_blank').focus();
+});
+
+//Youtube: Reading the rules of baba is you
+var southWestCord = L.latLng(75.642, 102.799);
+var northEatCord = L.latLng(77.706, 114.818);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=Jf5O8S5GiOo", '_blank').focus();
+});
+
+//Youtube: How Baba is You works
+var southWestCord = L.latLng(75.642, 117.015);
+var northEatCord = L.latLng(77.706, 129.221);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=7zLwa4bztWs", '_blank').focus();
+});
+
+//Youtube: Talk and Dev
+var southWestCord = L.latLng(75.642, 131.396);
+var northEatCord = L.latLng(77.706, 143.147);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=v7ICYHL5MD4", '_blank').focus();
+});
